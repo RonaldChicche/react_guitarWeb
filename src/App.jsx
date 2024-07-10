3b6247251b1f27f4d3a2fbdeaaa32897957f7a9e
RonaldC <desarrollo.it02@sokso.com>
2024-07-10 00:55:07 -0500

    functions and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,72 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Guitar from './components/Guitar'
+import { db } from './data/db'
 
 
 function App() {
 
   // State 
-  const [auth , setAuth] = useState([])
+  const [data , setData] = useState(db)
+  const [cart, setCart] = useState([])
 
-  console.log(auth)
+  const MAX_QUANTITY = 5
+
+  // function
+  function addToCart(item) {
+    const itemExist = cart.findIndex(guitar => guitar.id === item.id)
+    if (itemExist >= 0) {
+      const updatedCart = [...cart]
+      updatedCart[itemExist].quantity++
+      setCart(updatedCart)
+    } else {
+      console.log('El item no existe')
+      item.quantity = 1
+      setCart(prevCart => [...prevCart, item])
+    }    
+  }
+
+  function removeFromCart(id) {
+    // console.log('removing item', id)
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function increaseQuantity(id) {
+    console.log('increasing quantity', id)
+    const updatedCart = cart.map(item => {
+      if (item.id === id && item.quantity < MAX_QUANTITY) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
 
   return (
     <>
     
-    <Header />
+    <Header 
+      cart={cart}
+      removeFromCart={removeFromCart}
+      increaseQuantity={increaseQuantity}
+    />
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
+            {data.map((guitar) => (
+                <Guitar 
+                    key={guitar.id} 
+                    guitar={guitar}
+                    cart={cart}
+                    setCart={setCart}
+                    addToCart={addToCart}
+                />
+            ))}
 
         </div>
     </main>
